9be5b93fa0a92479c90b28da6ba2ddb848138053
Neeraj Kumar <nkurmbanshi@yahoo.com>
2024-03-19 13:05:15 +0530

    1903-first-commit

--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
